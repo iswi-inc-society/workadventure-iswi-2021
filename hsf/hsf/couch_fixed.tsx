15a64cb4036dd5e37c6c1246ca43354d8a04a1eb
Niklas Loos <niklas.loos@live.com>
2021-05-15 18:20:49 +0200

    add hsf

---
+++ hsf/hsf/couch_fixed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="couch_fixed" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../tiles/couch_fixed.png" width="128" height="128"/>
+</tileset>
